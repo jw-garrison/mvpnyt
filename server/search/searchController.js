bb997c3d2f57018237f34bc8595a51e8ea6a9d4f
JW Garrison <jwolfgarrison@gmail.com>
2016-11-29 10:58:15 -0500

    Add console.log for API key

--- server/search/searchController.js
+++ server/search/searchController.js
@@ -2,6 +2,7 @@ var request = require('request');
 var key = process.env.API_KEY || require('../config/api').API_KEY;
 
 exports.searchNYT = function (req, res) {
+  console.log(key);
   var q = '"' + req.body.q + '"';
   request.get({
     url: "https://api.nytimes.com/svc/search/v2/articlesearch.json",
@@ -11,7 +12,7 @@ exports.searchNYT = function (req, res) {
       'fl': "web_url,byline,snippet,multimedia,headline,lead_paragraph"
     },
   }, function(err, response, body) {
-    console.log(body);
+    console.log(body.response.body);
     body = JSON.parse(body);
     res.send(body.response.docs);
   });
