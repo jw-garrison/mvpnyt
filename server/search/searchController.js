e3faed55d354da4d5a0d011faa24957e3fc48a2c
JW Garrison <jwolfgarrison@gmail.com>
2016-11-29 11:02:18 -0500

    Add console.log

--- server/search/searchController.js
+++ server/search/searchController.js
@@ -2,7 +2,7 @@ var request = require('request');
 var key = process.env.API_KEY || require('../config/api').API_KEY;
 
 exports.searchNYT = function (req, res) {
-  console.log(key);
+  console.log("API KEY, expecting: f431b82fab3743caaf8c33f2298a16f2", key);
   var q = '"' + req.body.q + '"';
   request.get({
     url: "https://api.nytimes.com/svc/search/v2/articlesearch.json",
@@ -12,8 +12,8 @@ exports.searchNYT = function (req, res) {
       'fl': "web_url,byline,snippet,multimedia,headline,lead_paragraph"
     },
   }, function(err, response, body) {
-    console.log(body);
-    console.log(body.response);
+    console.log('++++++++body:' body);
+    console.log('++++++++body.response:' body.response);
     body = JSON.parse(body);
     res.send(body.response.docs);
   });
